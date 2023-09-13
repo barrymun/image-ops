10e4169528b8b7132fa0f1e75fcf9cc5ec5966ef
Neil Murphy <barrymun@tcd.ie>
2023-09-13 14:47:33 +0100

    switching to yarn instead of bun

--- build.js
+++
@@ -1,9 +0,0 @@
-await Bun.build({
-  entrypoints: ["./src/index.ts"],
-  outdir: "./dist",
-  target: "browser",
-  format: "esm",
-  splitting: true,  // https://bun.sh/docs/bundler#splitting
-  sourcemap: "none",
-  minify: true,
-});
